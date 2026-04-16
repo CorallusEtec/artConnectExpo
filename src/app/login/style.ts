cd3a115849af78fc033e6edcbe66c511ce5d4165
Muri XD <DxzMuri>
2026-04-16 15:49:39 -0300

    Arrumei os erros que tavam na TelaLogin

---
+++ src/app/login/style.ts
@@ -0,0 +1,17 @@
+import { gStyles } from "@/style/gStyle";
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    gap: 10,
+    justifyContent: 'flex-start',
+    marginTop: -50,
+  },
+  view1: {
+    gap: 20,
+    alignItems: "center"
+  },
+  
+});
