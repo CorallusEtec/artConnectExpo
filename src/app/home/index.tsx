4aa9a5b344bb4dffa7deefd85421f059a81410fc
Rafael <rafaelsoutodemoura09@gmail.com>
2026-05-11 16:28:12 -0300

    listagem de publicacoes funcionando na home

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,31 +1,32 @@
 import { Post } from "@/components/Post";
 import { Action } from "@/components/Post/Action";
 import { TextButton } from "@/components/TextButton";
+import PublicacoesService from "@/services/PublicacoesService";
 import { gStyles } from "@/style/gStyle";
 import { AntDesign, Feather, FontAwesome } from "@expo/vector-icons";
-import { useFocusEffect } from "expo-router";
-import { useCallback, useState } from "react";
+import { useEffect, useState } from "react";
 import { ActivityIndicator, FlatList, Image, TouchableOpacity, View } from "react-native";
 import { style } from "./style";
 
 export default function Home() {
-  const [post, setPost] = useState<any>();
+  const [legenda, setLegenda] = useState("");
+  const [midia, setMidia] = useState([]);
+
+  const [publicacoes, setPublicacoes] = useState([]);
   const [load, setLoad] = useState(true);
 
-  async function getPost() {
-    const data = await fetch("https://dummyjson.com/posts");
-    return data.json();
-  }
-
-  useFocusEffect(
-    useCallback(() => {
-      (async () => {
-        const data = await getPost();
-        setPost(data);
-        setLoad(false);
-      })();
-    }, []),
-  );
+  useEffect(() => {
+    async function carregar() {
+      try{
+        const data = await PublicacoesService.listar();
+        setPublicacoes(data);
+      } catch (Erro) {
+        console.log(Erro);
+      }
+    }
+    setLoad(false);
+    carregar();
+  }, []);
 
   if (load) return <ActivityIndicator size={"large"} />;
 
@@ -41,29 +42,37 @@ export default function Home() {
         </TouchableOpacity>
       </View>
       <View style={style.container}>
-        
+
         <FlatList
-          data={post.posts}
-          keyExtractor={(post) => post.id}
-          contentContainerStyle={{gap: 30}}
+          data={publicacoes}
+          keyExtractor={(item: any) => String(item.id)}
           renderItem={({ item }) => (
-            <Post.root>
-              <Post.header nomePerfil="João" data={new Date("2026-04-16T10:30:00")}>
-                <Post.headerActions>
-                  <TextButton title="Seguir" theme="secondary" />
-                </Post.headerActions>
-              </Post.header>
-              <Post.legend data={item.body} />
-              <Post.actions>
-                <Action insight={item.reactions.likes}>
-                  <FontAwesome name="heart-o" size={24} color={gStyles.vermelho[400]} />
-                </Action>
-                <Action insight={item.views}>
-                  <Feather name="message-circle" size={24} color={gStyles.cinza[600]} />
-                </Action>
-              </Post.actions>
-            </Post.root>
-          )}
+          <Post.root>
+            <Post.header
+              nomePerfil={item.autor?.nome ?? "Usuário"}
+              data={new Date(item.dataPublicacao)}
+            >
+              <Post.headerActions>
+                <TextButton title="Seguir" theme="secondary" />
+              </Post.headerActions>
+            </Post.header>
+
+            <Post.legend data={item.legenda} />
+            {item.urlMidia && (
+              <Post.image url={item.urlMidia} />
+            )}
+            
+            <Post.actions>
+              <Action insight={0}>
+                <FontAwesome name="heart-o" size={24} color={gStyles.vermelho[400]} />
+              </Action>
+
+              <Action insight={0}>
+                <Feather name="message-circle" size={24} color={gStyles.cinza[600]} />
+              </Action>
+            </Post.actions>
+          </Post.root>
+        )}
         />
         
       </View>
