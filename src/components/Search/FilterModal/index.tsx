3088a9f03d3b1b6ac1b7bcfda0a70dc3a2378cf8
NkAndre <andre270708@gmail.com>
2026-06-08 20:16:12 -0300

    arrumei no code erro de importacao

--- src/components/Search/FilterModal/index.tsx
+++ src/components/Search/FilterModal/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { View, Text, ScrollView } from "react-native";
 import { Modal, Portal, TextInput, Button, IconButton } from "react-native-paper";
 import { style } from "./style";
-import { AppUtils } from "../../services/AppUtils";
-import { FiltrosState, FilterModalProps } from "./types";
+import { AppUtils } from "../../../services/AppUtils";
+import { FiltrosState, FilterModalProps } from "../types";
 
 const executarAcaoFiltrar = (executarBusca: () => void, onClose: () => void) => {
   executarBusca();
