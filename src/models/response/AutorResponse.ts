24f5f8a53424d91d2b4e53a1e32b6d47346bad10
SamuMeneDev <samuelsantiago2222@gmail.com>
2026-06-11 12:54:42 -0300

    Foto de perfil nas publicações

--- src/models/response/AutorResponse.ts
+++ src/models/response/AutorResponse.ts
@@ -3,6 +3,7 @@ import { TipoConta } from "../enumeration/enumeration";
  * Interface para tipar autor de publicação e comentário
  */
 export interface AutorResponse {
+  fotoPerfilUrl: string;
   id: number;
   nome: string;
   tipoConta: TipoConta;
