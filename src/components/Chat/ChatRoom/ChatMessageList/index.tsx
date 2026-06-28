6e3fcd13cb536f3374a3383a93d7cb236a8deabf
SamuMeneDev <samuelsantiago2222@gmail.com>
2026-06-28 00:03:37 -0300

    Paginação inversa em andamento

--- src/components/Chat/ChatRoom/ChatMessageList/index.tsx
+++ src/components/Chat/ChatRoom/ChatMessageList/index.tsx
@@ -1,16 +1,30 @@
 import { useGetChatMessageHistory } from "@/services/ChatService";
 import { useLocalSearchParams } from "expo-router";
 import { FlatList } from "react-native";
+import { ActivityIndicator } from "react-native-paper";
 import { ChatMessage } from "../ChatMessage";
 import { style } from "./style";
 
 export function ChatMessageList() {
   const { id } = useLocalSearchParams();
-  const { data, isLoading } = useGetChatMessageHistory(Number(id));
+  const { data, hasNextPage, fetchNextPage, isFetchingNextPage, isLoading } =
+    useGetChatMessageHistory(Number(id));
+
+  function renderFooter() {
+    if (!isFetchingNextPage) return null;
+    return <ActivityIndicator size={"large"} />;
+  }
 
   if (isLoading) return <></>;
   return (
     <FlatList
+      onEndReachedThreshold={0.1}
+      ListFooterComponent={renderFooter}
+      onEndReached={() => {
+        if (hasNextPage && !isFetchingNextPage) {
+          fetchNextPage();
+        }
+      }}
       style={style.container}
       inverted
       data={data?.pages.flatMap((page) => page.data.content) ?? []}
